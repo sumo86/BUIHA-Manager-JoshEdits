7e71e5d21634662b789f011afd7095e2f33c0650
[dyad] <git@dyad.sh>
2025-07-09 00:04:52 +0100

    [dyad] Increased base player abilities for Checking 1 division - wrote 1 file(s)

--- src/lib/leagueUtils.ts
+++ src/lib/leagueUtils.ts
@@ -1,6 +1,6 @@
 export const divisionTiers: { [key: string]: { skater: number, goalie: number, step: { skater: number, goalie: number } } } = {
-    'Checking 1': { skater: 500, goalie: 230, step: { skater: 25, goalie: 12 } }, // Increased significantly
-    'Checking 2': { skater: 420, goalie: 190, step: { skater: 25, goalie: 12 } }, // Increased slightly
+    'Checking 1': { skater: 520, goalie: 240, step: { skater: 25, goalie: 12 } }, // Further increased
+    'Checking 2': { skater: 420, goalie: 190, step: { skater: 25, goalie: 12 } },
     'Non-Checking 1': { skater: 350, goalie: 155, step: { skater: 28, goalie: 13 } }, 
     'Non-Checking 2': { skater: 310, goalie: 135, step: { skater: 30, goalie: 15 } },
     'Non-Checking 3': { skater: 270, goalie: 115, step: { skater: 35, goalie: 18 } },
