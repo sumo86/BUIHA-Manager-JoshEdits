05a357c903bac223d52992b18f790098d6f96a00
[dyad] <git@dyad.sh>
2025-07-10 01:22:01 +0100

    [dyad] Adjusted division tiers for Non-Checking 3 and Checking 2 to reduce the prevalence of 5-star players - wrote 1 file(s)

--- src/lib/leagueUtils.ts
+++ src/lib/leagueUtils.ts
@@ -1,7 +1,7 @@
 export const divisionTiers: { [key: string]: { skater: number, goalie: number, step: { skater: number, goalie: number } } } = {
     "Checking 1":       { skater: 335, goalie: 155, step: { skater: 80, goalie: 38 } },
-    "Checking 2":       { skater: 269, goalie: 125, step: { skater: 30, goalie: 14 } },
+    "Checking 2":       { skater: 285, goalie: 135, step: { skater: 30, goalie: 14 } }, // Increased skater/goalie values
     "Non-Checking 1":   { skater: 244, goalie: 113, step: { skater: 20, goalie: 10 } },
     "Non-Checking 2":   { skater: 222, goalie: 103, step: { skater: 19, goalie: 9 } },
-    "Non-Checking 3":   { skater: 206, goalie: 96,  step: { skater: 20, goalie: 9 } },
+    "Non-Checking 3":   { skater: 220, goalie: 105, step: { skater: 20, goalie: 9 } }, // Increased skater/goalie values
 };
